79a58b20fad37273ca3cbd9f0d0a00cf9eb76885
JoeTelaa0 <47280152+The1TrueJoe@users.noreply.github.com>
2021-06-14 10:35:38 -0400

    Update CMD.bat

--- CMD.bat
+++ CMD.bat
@@ -1,12 +1,12 @@
 :: Written By Joseph Telaak
 :: DO NOT CHANGE
 
-:: I AM NOT RESPONSIBLE FOR ANY DAMAGE DONE BY THIS PROGRAM
+:: I AM NOT RESPONSIBLE FOR ANY UNAUTHORIZED USE OF THIS PROGRAM
 
 @echo off
 title Joseph Telaak's Command Prompt
 ver
-echo Copyright (c) 2019 Joseph Telaak
+echo Copyright (c) 2018!Joseph Telaak
 color 0a
 cd ..
 echo.
